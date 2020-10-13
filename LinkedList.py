141429afdf2a875674fc58587ffb489760b124d5
Mehedi's Mac <macbookpro@MacBooks-MacBook-Pro.local>
2020-10-14 00:34:56 +0600

    Linked list insert inside

--- LinkedList.py
+++ LinkedList.py
@@ -44,6 +44,13 @@ class Node:
         return head
 
     @staticmethod
+    def insert_into_list(head, item):
+        #  we need to add new node after the passing node
+        new_node = Node().create_node(item, head.next)
+        head.next = new_node
+        return head
+
+    @staticmethod
     def insert_node_at_first(head, item):
         #  For add in 1st at Linked List, just create node [Item, head] , new item with head reference
         #  Then change head position.
@@ -52,19 +59,19 @@ class Node:
 
     @staticmethod
     def insert_node_at_last(head, item):
+        cur_node = head
         inserted_node = Node().create_node(item, None)
-        if head is None:
+        if cur_node is None:
             return inserted_node
 
         #  For add in last at Linked List, We need to iterate list to get node which has null reference
         #  then just create node [Item, None]
         #  after that add this node to the last item
-        while head.next is not None:
-            head = head.next
+        while cur_node.next is not None:
+            cur_node = cur_node.next
             #  Complexity O(n)
-
-        if head.next is None:
-            head.next = inserted_node
+        else:
+            cur_node.next = inserted_node
 
         return head
 
@@ -78,6 +85,15 @@ def get_last_node(head):
     return c_head
 
 
+def print_linked_list(head, text):
+    print(f"___________________{text}____________________")
+    tv = []
+    while head is not None:
+        tv.append(head.data)
+        head = head.next
+    print(tv)
+
+
 if __name__ == '__main__':
     new_node4 = Node().create_node(8, None)
 
@@ -88,7 +104,8 @@ if __name__ == '__main__':
     new_node1 = Node().create_node(10, new_node2)
 
     head = new_node1
-    print(head.data)
+
+    print_linked_list(head, "Before any operation")
 
     #  Remove node
     # after removed node 3, node 3  is the reference in head.
@@ -97,12 +114,27 @@ if __name__ == '__main__':
     head = Node().remove_node(head, new_node2)
     print(f"after removed 2nd node is : {head.next.data}")
 
+    print_linked_list(head, "After remove node 2")
+
     #  Insert node at 1st
     head = Node().insert_node_at_first(head, 15)
+    head = Node().insert_node_at_first(head, 39)
     print(f"1st item after insert at 1st : {head.data}")
     print(f"2nd item after insert at 1st :{head.next.data}")
+
+    print_linked_list(head, "After insert at 1st")
 
     #  Insert node at last
     print(f"Last item before insert at last : {get_last_node(head).data}")
     head = Node().insert_node_at_last(head, 100)
     print(f"Last item after insert at last :{get_last_node(head).data}")
+
+    print_linked_list(head, "After insert at last")
+
+    #  Insert into specific position
+    print(f"2nd item before insert at middle : {head.next.data}")
+    head = Node().insert_into_list(head, 50)
+    print(f"2nd item after insert at middle :{head.next.data}")
+
+    print_linked_list(head, "After insert inside last")
+
